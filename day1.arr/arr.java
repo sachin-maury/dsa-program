9e5cc63cd25a379c6cb7268377be7660ff6b6099
Sachin Maurya <kumarsachin67671@gmail.com>
2024-02-03 22:59:54 +0530

    call by referance

---
+++ day1.arr/arr.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class arr {
+    public static void update(int marks[]) {
+        for(int i=0; i<marks.length; i++){
+            marks[i] =marks[i]+1;
+        }
+    }
+    public static void main(String[] args) {
+       int marks[] ={56,45,58};
+       update(marks);
+       for(int i=0; i<marks.length; i++){
+        System.out.print(marks[i]+" ");
+        System.out.println();
+       }
+
+    }
+}
