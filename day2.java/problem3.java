24e4e4bda207e076ee57187dcce7687543ad5461
Sachin Maurya <kumarsachin67671@gmail.com>
2024-02-05 23:19:54 +0530

    Comparision b/w two no

---
+++ day2.java/problem3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class problem3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your first number:");
+        int num1 =sc.nextInt();
+        System.out.println("Enter your second number:");
+        int num2 =sc.nextInt();
+        sc.close();
+        if(num1==num2)
+        System.out.println("both are equal:");
+        else if(num1>num2)
+        System.out.println(num1+"is greater:");
+        else
+        System.out.println(num2+"is greater:");
+        }
+    }
+    
+
