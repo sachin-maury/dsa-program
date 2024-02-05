cb35d1993b3a3ca414993986fb9266bde720c3a8
Sachin Maurya <kumarsachin67671@gmail.com>
2024-02-05 23:38:12 +0530

    infinite do while loop

---
+++ problem5.java
@@ -0,0 +1,10 @@
+public class problem5 {
+    public static void main(String[] args) {
+        int n=10;
+        do{
+            System.out.println("hello mummy&papa");
+            
+        }
+        while(n<15);
+    }
+}
