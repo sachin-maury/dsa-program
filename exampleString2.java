ec3f796b430d1e66640506ad67f16952c1b7f71e
Sachin Maurya <kumarsachin67671@gmail.com>
2024-03-27 22:12:10 +0530

    String example 2

---
+++ exampleString2.java
@@ -0,0 +1,6 @@
+public class exampleString2 {
+    public static void main(String[] args) {
+        String sc = new String("Hello how are you ?. I think you are fine:");
+        System.out.println(sc);
+    }
+}
