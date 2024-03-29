5a76110c2b4390aba7033b63b15d0cb6700918be
Sachin Maurya <kumarsachin67671@gmail.com>
2024-03-29 22:59:42 +0530

    Lower and Upper case in java

---
+++ usinLowerCase.java
@@ -0,0 +1,18 @@
+public class usinLowerCase {
+    public static void main(String[] args) {
+
+        String name="HELLO";
+
+        String sc = name.toLowerCase();  
+
+        System.out.println(sc);
+        
+        // To upper case
+
+        String name2 = "hollo";
+
+        String st = name2.toUpperCase();
+        
+        System.out.println(st);
+    }
+}
