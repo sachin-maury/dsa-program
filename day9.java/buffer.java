d5a54391b55a9a297594d163d5d7d5fc8d9e3092
Sachin Maurya <kumarsachin67671@gmail.com>
2024-03-29 22:32:23 +0530

    using buffer to print reverse string

---
+++ day9.java/buffer.java
@@ -0,0 +1,6 @@
+public class buffer {
+    public static void main(String[] args) {
+        StringBuffer sc = new StringBuffer("PLEASE HELP ME");
+        System.out.println(sc.reverse());
+    }
+}
