36cf2813f84a397fddf2ae59980fe74b61e9f264
Sachin Maurya <kumarsachin67671@gmail.com>
2024-03-31 23:51:57 +0530

    practice set 5 java

---
+++ day11.java/pattern1.java
@@ -0,0 +1,11 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=4; i++){
+            for(int j=i-1; j<3; j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
