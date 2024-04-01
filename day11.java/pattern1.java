750b2778e721720424f73a27f60629688559a8c2
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-01 21:37:12 +0530

    Solid pattern

--- day11.java/pattern1.java
+++ day11.java/pattern1.java
@@ -2,10 +2,10 @@ public class pattern1 {
     public static void main(String[] args) {
         
         for(int i=1; i<=4; i++){
-            for(int j=i-1; j<3; j++){
+            for(int j=i-1; j<4; j++){
                 System.out.print("*");
             }
-            System.out.println("*");
+            System.out.println();
         }
     }
 }
