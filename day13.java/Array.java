b3f126e85b85d6fef7bfa2d8828afe33a679c536
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-03 22:08:08 +0530

    Print Array

---
+++ day13.java/Array.java
@@ -0,0 +1,42 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[];
+        arr=new int [3];
+        arr[0] =95;
+        arr[1] =97;
+        arr[2] =98;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+
+
+
+        // 2nd methods
+        System.out.println("This is another example: 2");
+
+
+        int marks[] =new int[3];
+        marks[0] =95;
+        marks[1] =97;
+        marks[2] =98;
+        
+        for(int i=0; i<3; i++){
+           
+           System.out.println(marks[i]);
+        }
+
+
+
+
+        // Third methods in arry;
+
+
+        System.out.println("This is 3rd method in implement arry:");
+
+
+        int mark [] ={95 , 96  ,98};
+        for(int i=0; i<3; i++){
+        System.out.println(mark[i]);
+        }
+    }
+}
