a00bb7622ece53053ce9347e013ac6c817508a60
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-04 22:42:22 +0530

    Sum of Two numbers

---
+++ day14.java/sumOfTwoNo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class sumOfTwoNo {
+     
+    public static int printNumber(int a ,int b){
+        
+      int sum=a+b;
+    
+      return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter your number:");
+
+        Scanner sc = new Scanner(System.in);
+      
+        int a=sc.nextInt();
+
+        int b=sc.nextInt();
+
+        int sum =printNumber(a, b);
+
+        System.out.println(sum);
+
+    }
+}
