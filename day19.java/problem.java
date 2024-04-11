33976bbe1a446dc4ff38804e0dd8d934ed7c7f43
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-11 20:22:20 +0530

    printArray

---
+++ day19.java/problem.java
@@ -0,0 +1,20 @@
+public class problem {
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,9,5,2,9,4,2};
+        for(int i=0; i<arr.length+1;i++){
+            for(int j=0; j<arr.length+i+1;j++){
+                if(arr[j] < arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] =temp;
+                }
+            }
+        }
+    }
+}
