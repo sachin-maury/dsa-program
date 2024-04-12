a91e5762c8f281f26a277953c12a87308d81b735
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-12 22:23:59 +0530

    proble 3

---
+++ day20.java/element.java
@@ -0,0 +1,24 @@
+public class element {
+
+    public static void printElementArray(int a[]){
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int a[] = {45,70,41,32,40};
+        for(int i=0; i<a.length-1; i++){
+            for(int j=0; j<a.length-i-1; j++){
+                if(a[i] < a[j+1]){
+                    //swap 
+                    int temp = a[i];
+                    a[i] =a[j+1];
+                    a[j+1] =temp;
+            
+                }
+            }
+        }
+        printElementArray(a);
+    }
+}
