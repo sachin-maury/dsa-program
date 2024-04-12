f9b0b968f85d2d41d10579800dd3d03331074d9e
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-12 22:26:20 +0530

    problem3

--- day20.java/element.java
+++ day20.java/element.java
@@ -10,10 +10,10 @@ public class element {
         int a[] = {45,70,41,32,40};
         for(int i=0; i<a.length-1; i++){
             for(int j=0; j<a.length-i-1; j++){
-                if(a[i] < a[j+1]){
+                if(a[j] > a[j+1]){
                     //swap 
-                    int temp = a[i];
-                    a[i] =a[j+1];
+                    int temp = a[j];
+                    a[j] =a[j+1];
                     a[j+1] =temp;
             
                 }
