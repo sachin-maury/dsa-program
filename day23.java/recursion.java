83fd25a1aca12ee20585efa7e04a781b5878ad2e
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-15 23:33:19 +0530

    Recursion

---
+++ day23.java/recursion.java
@@ -0,0 +1,16 @@
+public class recursion {
+
+    public static void printRecursion(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+            printRecursion(n-1);
+    }
+
+    public static void main(String[] args){
+        int n = 5; 
+        printRecursion(n);
+        
+    }
+}
