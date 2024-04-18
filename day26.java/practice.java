bf9a7f4450af7f17b4275dbf638f7f12adfd3030
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-18 23:48:07 +0530

    practice problem

---
+++ day26.java/practice.java
@@ -0,0 +1,13 @@
+public class practice {
+    public static void main(String[] args) {
+        int n =5;
+        int pos=3;
+        int bitmask=1<<pos;
+        if((bitmask & n)==0){
+           System.out.println("Bit was zero:");
+        }
+        else{
+            System.out.println("Bit was one :");
+        }
+    }
+}
