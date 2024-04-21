e5f03877ee437b5c93b0dceb60c8149df2f745ee
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-21 22:03:58 +0530

    problems

---
+++ day29.java/problems.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class problems {
+    public static void printFactorial(int n){
+        if(n<0){
+            System.out.println("Invalid Number");
+            return; // in middle loop we want to end then we are use return
+        }
+
+        int factorial=1;
+
+        for(int i=n; i>=1; i--){
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+        return; //we are writing to loop is end here
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printFactorial(n);
+
+    }
+}
