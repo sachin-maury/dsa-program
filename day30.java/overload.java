968069606a0f38715d1b201d7eac93e7e7accb21
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-22 23:03:25 +0530

    Method  Overriding

--- day30.java/overload.java
+++ day30.java/overload.java
@@ -7,17 +7,14 @@ class calculator{
     public int add(int n1,int n2){
         return n1+n2;
     }
-    public int mul(int n1,int n2){
-         return n1*n2;
-    }
+   
 }
 public class overload {
     public static void main(String[] args) {
         calculator obj = new calculator();
         int r1 = obj.add(3,4);
-        int r2= obj.mul(3,4);
+        
         System.out.println(r1);
-        System.out.println(r2);
-
+        
     }
 }
