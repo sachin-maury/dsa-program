7b956c72f280165ea3291b5308db36623305f2e0
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-02 20:37:34 +0530

    Object call In Java

---
+++ day40.java/classA.java
@@ -0,0 +1,5 @@
+public class classA {
+    public int add(int a,int b){
+        return a+b;
+    }
+}
---
+++ day40.java/classB.java
@@ -0,0 +1,5 @@
+public class classB {
+    public int multiply(int a, int b){
+        return a*b;
+    }
+}
