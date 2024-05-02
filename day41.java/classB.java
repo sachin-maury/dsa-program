afbf34e921a25ae8f6db5813fd2dc1e6bb5db2ea
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-02 22:34:53 +0530

    constructor override program

---
+++ day41.java/classB.java
@@ -0,0 +1,19 @@
+public class classB {
+
+    public int multiply(int a ,int b){
+        return a*b;
+    }
+    public classB(){
+        // default constructor
+    }
+    public classB(int rollno){
+        // parameterize constructor
+    }
+    
+    public classB(String name){
+         // parameterize constructor
+    }
+    public classB(String name,int rollno){
+       // parameterize constructor  
+    }
+}
---
+++ day41.java/practise.java
@@ -0,0 +1,14 @@
+public class practise {
+    public static void main(String[] args) {
+        practise practise = new practise();
+        practise.cont();
+    }
+    public void cont(){
+          int sum = super.add(11, 11);
+          System.out.println("The sum is :" +sum );
+          classB b;
+          b = new classB();
+        int product=  b.multiply(11, 11);
+        System.out.println("The poduct is : " +product);
+    }
+}
