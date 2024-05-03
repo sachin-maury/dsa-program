54146f7fc0372b0798ee153675981e071ef84838
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-03 21:06:09 +0530

    loops

---
+++ day42.java/loops.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class loops {
+
+    public static void main(String[] args) {
+        for(int i=0; i<=10; i--){
+            
+            ArrayList<String> arrayNames = new ArrayList<>();
+            arrayNames.add("Mogan");
+            arrayNames.add("Arti");
+            arrayNames.add("Heroin");
+            arrayNames.add("sonu");
+           System.out.println(arrayNames);
+        }
+    }
+}
